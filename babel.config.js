1efa9cfa8539cf148ab77c5935de32edef4a81c1
Jagannath-Developer <raojagannath11@gmail.com>
2024-10-14 02:57:41 +0530

    install watermelon

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = [];
+  const plugins = [['@babel/plugin-proposal-decorators', { legacy: true }]];
 
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
+      'module:metro-react-native-babel-preset',
+    ],
 
     plugins,
   };
